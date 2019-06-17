f2c088b20c1e6eec617bcb2643dcab25d5173d37
Arthur Jahnke Hoch <m96600@acad.unisc.br>
2019-06-17 19:29:58 -0300

    .

---
+++ src/pages/Comodo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Comodo.scss';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Row gutter={8}>
+                    <Col span={12}>
+
+                    </Col>
+                    <Col span={12}>
+
+
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
+
+export default Home;
