8e646f08f9dd12371d8c4fe91627ee70feb5eb0b
William <william.lucas.mehler@gmail.com>
2019-06-17 21:40:47 -0300

    Updates

---
+++ precache-manifest.41d66cc8cf428675fbdbcc921abc66a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cca85e00b274f44b8418ea022a9b94",
+    "url": "/producao-web/index.html"
+  },
+  {
+    "revision": "fed0870e69c873719154",
+    "url": "/producao-web/static/css/2.59ca6ef4.chunk.css"
+  },
+  {
+    "revision": "a2e7c27df2876f12a02e",
+    "url": "/producao-web/static/css/main.22e48dcc.chunk.css"
+  },
+  {
+    "revision": "fed0870e69c873719154",
+    "url": "/producao-web/static/js/2.46eb1ae7.chunk.js"
+  },
+  {
+    "revision": "a2e7c27df2876f12a02e",
+    "url": "/producao-web/static/js/main.311c4e83.chunk.js"
+  },
+  {
+    "revision": "d7d07e5018289c38291e",
+    "url": "/producao-web/static/js/runtime~main.8eb61003.js"
+  },
+  {
+    "revision": "f163ecffb0f22b2593d8ebaa671eb537",
+    "url": "/producao-web/static/media/img-casa-cima.f163ecff.jpeg"
+  },
+  {
+    "revision": "2ed7f03b63186cb5cbbec3cd398a2a99",
+    "url": "/producao-web/static/media/solitaria-logo.2ed7f03b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.264e08b0279bfb45393811ea307a4798.js"
+  "/producao-web/precache-manifest.41d66cc8cf428675fbdbcc921abc66a6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/producao-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
