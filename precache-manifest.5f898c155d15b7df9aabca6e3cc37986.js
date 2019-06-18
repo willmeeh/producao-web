63cd7b7caf28f1b12ead6537d7d3bbc7d7297d36
William <william.lucas.mehler@gmail.com>
2019-06-17 21:31:22 -0300

    Updates

---
+++ precache-manifest.5f898c155d15b7df9aabca6e3cc37986.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4ab65421dbcbdb99cfa9a9bf0fc29e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "101d691f8152bb5d94b2",
+    "url": "/static/css/2.59ca6ef4.chunk.css"
+  },
+  {
+    "revision": "402765b013d39ce80a59",
+    "url": "/static/css/main.b6f126a6.chunk.css"
+  },
+  {
+    "revision": "101d691f8152bb5d94b2",
+    "url": "/static/js/2.364062c2.chunk.js"
+  },
+  {
+    "revision": "402765b013d39ce80a59",
+    "url": "/static/js/main.17bba8f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f163ecffb0f22b2593d8ebaa671eb537",
+    "url": "/static/media/img-casa-cima.f163ecff.jpeg"
+  },
+  {
+    "revision": "2ed7f03b63186cb5cbbec3cd398a2a99",
+    "url": "/static/media/solitaria-logo.2ed7f03b.png"
+  }
+]);
