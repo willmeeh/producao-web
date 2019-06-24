17520c27a1745344c8437aa1e71d1497be1114d8
Arthur Jahnke Hoch <m96600@ACAD.UNISC.BR>
2019-06-24 20:10:41 -0300

    Updates

---
+++ precache-manifest.8e73082804cc808bf0d7cfad63720cbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce30235abbe0b1fef559fb26d7788ad",
+    "url": "/producao-web/index.html"
+  },
+  {
+    "revision": "84fe2801e99b49f35e64",
+    "url": "/producao-web/static/css/2.59ca6ef4.chunk.css"
+  },
+  {
+    "revision": "56866727ed104957c3f0",
+    "url": "/producao-web/static/css/main.d926196f.chunk.css"
+  },
+  {
+    "revision": "84fe2801e99b49f35e64",
+    "url": "/producao-web/static/js/2.22da08ee.chunk.js"
+  },
+  {
+    "revision": "56866727ed104957c3f0",
+    "url": "/producao-web/static/js/main.d1596027.chunk.js"
+  },
+  {
+    "revision": "d7d07e5018289c38291e",
+    "url": "/producao-web/static/js/runtime~main.8eb61003.js"
+  },
+  {
+    "revision": "f163ecffb0f22b2593d8ebaa671eb537",
+    "url": "/producao-web/static/media/img-casa-cima.f163ecff.jpeg"
+  },
+  {
+    "revision": "2ed7f03b63186cb5cbbec3cd398a2a99",
+    "url": "/producao-web/static/media/solitaria-logo.2ed7f03b.png"
+  }
+]);
